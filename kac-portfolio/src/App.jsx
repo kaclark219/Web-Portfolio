626ca7400a90b5ee84726665f0bbd846ca42bdbf
Katelyn Clark <kaclark219@gmail.com>
2025-05-02 14:02:15 -0500

    environment setup

---
+++ kac-portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './style/style.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio!</h1>
+      <p>This site is using React and SCSS.</p>
+    </div>
+  );
+}
+
+export default App
