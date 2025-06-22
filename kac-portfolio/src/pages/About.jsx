f9d0d8f84871a3a92887ba617601b0d8d7903fa5
Katelyn Clark <kaclark219@gmail.com>
2025-06-21 20:57:32 -0500

    another lil me

--- kac-portfolio/src/pages/About.jsx
+++ kac-portfolio/src/pages/About.jsx
@@ -1,7 +1,11 @@
+import portrait from "../assets/about-me-portrait.PNG";
+
 function About() {
     return (
         <div>
-            <h1>Welcome to My Portfolio</h1>
+            <div className="about-drawing">
+                <img src={portrait} alt="pixel illustration of me" />
+            </div>
             <p>This is my about page content!</p>
         </div>
     );
